b2cbbcb2d40c15a8e131d40676edbf8b37674f36
Princess._.Yeol <67359781+JUNYEOL-GONG@users.noreply.github.com>
2023-09-19 05:53:51 +0900

    Update island_count_4963.cpp

--- Solved.ac/Solved.ac/island_count_4963.cpp
+++ Solved.ac/Solved.ac/island_count_4963.cpp
@@ -1,4 +1,8 @@
-#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
 
 struct Pos
 {
@@ -10,11 +14,8 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	vector<vector<int>> mapArray;
-
-	// 상 하 좌 우 / 좌상 우상 좌하 우하
-	// (x,y) Pos 배열 사용
-	vector<Pos> DirArray =
+	// 상 / 하 / 좌 / 우 / 좌상 / 우상 / 좌하 / 우하
+	const vector<Pos> DirArray =
 	{
 		{0, 1},
 		{0, -1},
@@ -30,41 +31,19 @@ int main()
 	while (true)
 	{
 		// 각 테스트 케이스의 첫째 줄에는 지도의 너비 w와 높이 h가 주어진다.
-		int w, h;
-		cout << "w와 h를 입력하세요" << '\n';
-		cin >> w; cin >> h;
-		cout << "=================" << '\n';
+		int w; cin >> w;
+		int h; cin >> h;
 
 		// 입력의 마지막 줄에는 0이 두 개 주어진다.
-		if (w == 0 && h == 0)
-		{
-			cout << "입력의 마지막 줄에는 0이 두 개 주어진다" << '\n';
-			break;
-		}
-
+		if (w == 0 && h == 0) break;
 		// w와 h는 50보다 작거나 같은 양의 정수
-		if (w < 1 || w > 50 || h < 1 || h > 50)
-		{
-			cout << "w와 h는 50보다 작거나 같은 양의 정수" << '\n';
-			break;
-		}
-
-		/*
-		// 지도 초기화
-		// 루프를 두 번 돌아야 해서 CPU 캐시 활용에서 불리할 수도 있다.
-		mapArray.resize(h);
-		for (int i = 0; i < h; ++i)
-		{
-			mapArray[i].resize(w);
-			for (int j = 0; j < w; ++j)
-			{
-				mapArray[i][j] = -1;
-			}
-		}
-		*/
+		if (w < 1 || w > 50 || h < 1 || h > 50) break;
 
 		// 효율적인 지도 초기화
+		vector<vector<int>> mapArray;
 		mapArray.resize(h, std::vector<int>(w, 0));
+
+		int islandCnt = 0;
 
 		// 지도를 그린다
 		for (int i = 0; i < h; ++i)			// 세로
@@ -78,36 +57,81 @@ int main()
 		}
 
 		// 섬의 개수를 구한다
-		// -> 뭐라도 있으면 그 경로는 하나의 섬
-		for (int i = 0; i < h; ++i)			// 세로
+		// -> 뭐라도 점이 있으면 그 경로는 하나의 섬
+		for (int i = 0; i < h; ++i)	// 세로
 		{
-			for (int j = 0; j < w; ++j)		// 가로
+			for (int j = 0; j < w; ++j)	// 가로
 			{
-				// 섬(1)의 경우만 체크한다 (이미 방문한 섬은 x)
+				// 만약 점이 하나 있으면 그 점을 기준으로 탐색을 시작한다 ==> "시작점"
 				if (mapArray[i][j] == 1)
 				{
-					// 모든 방향을 체크하자
-					for (int k = 0; k < DirArray.size(); ++k)
-					{
-						// 해당 방향에 있나?
-						if (mapArray[i - DirArray[k].y][j + DirArray[k].y])
-						{
+					// 처음 섬 탐색 시작 점을 curPos로 두고 시작한다
+					Pos curPos = { j,i };
 
+					// 지나온 곳은 map에서 기록한다
+					mapArray[curPos.y][curPos.x] = -1;
+
+					// 왔던곳을 돌아가기 위해서 지나온 점을 기억한다.
+					stack<Pos> posStack;
+					posStack.push(curPos);
+
+					// 섬이 어디까지 이어지는지 체크
+					while(true)
+					{
+						bool isFind = false;
+
+						// 모든 방향을 체크하자
+						for (int k = 0; k < DirArray.size(); ++k)
+						{
+							// 탐색하려는곳이 지도를 벗어나지 않는지 체크
+							if (curPos.y - DirArray[k].y >= 0 &&
+								curPos.x + DirArray[k].x >= 0 &&
+								curPos.y - DirArray[k].y < h &&
+								curPos.x + DirArray[k].x < w)
+							{
+								// 해당 방향에 있나?
+								if (mapArray[curPos.y - DirArray[k].y][curPos.x + DirArray[k].x] == 1)
+								{
+									// 현재 위치도 이동
+									curPos = { curPos.x + DirArray[k].x , curPos.y - DirArray[k].y };
+
+									// 이동한 위치를 스택에 넣는다
+									posStack.push(curPos);
+
+									// 지나온 곳은 map에서 기록한다
+									mapArray[curPos.y][curPos.x] = -1;
+
+									isFind = true;
+
+									break;
+								}
+							}
+						}
+
+						// 다 찾아봤는데도 없다..?
+						if (!isFind)
+						{
+							// 섬 탐색 종료
+							if (posStack.empty())
+							{
+								islandCnt++;
+								break;
+							}
+
+							// 뒤로 돌아간다
+							posStack.pop();
+
+							if (!posStack.empty())
+								curPos = posStack.top();
 						}
 					}
 				}
 			}
 		}
 
-
 		// 각 테스트 케이스에 대해서, 섬의 개수를 출력한다.
-		int result = 0;
-		cout << "-> 섬의 개수 : " << result << '\n';
-
-		cout << "=================" << '\n';
-
+		cout << islandCnt << '\n';
 	}
-
 
 	return -1;
 }
