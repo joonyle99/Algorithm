4bf4b2570ba67d524f5db1d6980157bbcfdd51ef
joonyle99 <gongjunyeol@gmail.com>
2023-10-20 02:39:09 +0900

    그리디 알고리즘 1차 완료

--- Solved.ac/Solved.ac/1541_Lost Bracket.cpp
+++ Solved.ac/Solved.ac/1541_Lost Bracket.cpp
@@ -1,15 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    // 잃어버린 괄호
+    // + - ()
+    // 이 식의 값을 '최소'로 만들려고 한다.
 
+    // 55-50+40
 
-	return 0;
+    // 5자리보다 많이 연속되는 숫자는 없다
+    // 수는 0으로 시작할 수 있다
+    // 입력으로 주어지는 식의 길이는 50보다 작거나 같다
+
+    // * `최소`를 만들기 위해서는 -가 커져야한다. *
+    // -에 괄호를 적절히 쳐서 -를 값을 크게 만든다
+
+    // ** '-' 뒤에 오는 식을 전부 뺄셈 처리하면 식의 값이 최소가 된다. **
+
+    // 문제점 1
+    // 문자열을 어떻게 정수로 처리할 것인가?
+
+    // 문제점 2
+    // 괄호를 어느 타이밍에 넣을 것이고, 어떻게 넣어야 하는걸까?
+
+    string input;
+    cin >> input;
+
+    string partialNum;
+    const int inputSize = static_cast<int>(input.size());
+    int res = 0;
+    bool isMinus = false;
+
+    for (int i = 0; i < inputSize; i++)
+    {
+        // 연산 기호가 나오면 계산한다.
+        if (input[i] == '-' || input[i] == '+')
+        {
+            res += (isMinus ? -1 : 1) * stoi(partialNum);
+            partialNum = "";
+        }
+        // 나머지 경우에는 부분 문자열에 수를 누적
+        else
+            partialNum += input[i];
+
+        // '-'가 한 번 나오면 그 뒤는 쭉 - 기호
+        if (input[i] == '-' && !isMinus)
+            isMinus = true;
+    }
+
+    // Input Data가 끝나면
+    res += (isMinus ? -1 : 1) * stoi(partialNum);
+    cout << res;
+
+    return 0;
 }
--- Solved.ac/Solved.ac/1931_Room assignments.cpp
+++ Solved.ac/Solved.ac/1931_Room assignments.cpp
@@ -4,11 +4,62 @@
 
 using namespace std;
 
+bool Compare(pair<int, int> a, pair<int, int> b)
+{
+	// 종료 시간이 같을 때, 시작 시간이 더 작은 순서대로 정렬되도록 한다.
+	if (a.second == b.second)
+		return a.first < b.first;
+
+	return a.second < b.second;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	// 회의실 배정
+
+	// 조건
+	// 1. 각 회의는 겹치지 않는다.
+	// 2. 회의는 시작하면 중단이 불가하다.
+	// 3. 회의가 끝나는 동시에 다음 회의가 시작될 수 있다.
+	// 4. 회의 시작 시간과 종료 시간이 같을 수 있다.
+
+	// e.g) (1, 4) -> (4, 4) 가능
+
+	// 첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다
+	int n; cin >> n;
+	int cnt = 0;
+	int time = 0;
+	vector<pair<int, int>> pairs(n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> pairs[i].first;
+		cin >> pairs[i].second;
+	}
+
+	// 회의 종료 시간이 적은 순서대로 정렬하기
+	std::sort(pairs.begin(), pairs.end(), Compare);
+
+	// 첫번째 회의
+	time = pairs[0].second;
+	cnt++;
+
+	for (int i = 1; i < n; i++)
+	{
+		// 종료 시간이 다음 회의 시작 시간보다 작거나 같으면
+		if (time <= pairs[i].first)
+		{
+			// 그 회의를 할 수 있고, 종료 시간을 저장한다.
+			time = pairs[i].second;
+			cnt++;
+		}
+	}
+
+	// 최대 사용할 수 있는 회의의 최대 개수를 출력
+	cout << cnt << endl;
 
 
 	return 0;
