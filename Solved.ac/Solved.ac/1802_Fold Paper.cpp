f2021e8ed71910fbdf12203fde6ec5ef427fa88d
joonyle99 <gongjunyeol@gmail.com>
2023-11-03 16:47:56 +0900

    종이 접기 해결중

--- Solved.ac/Solved.ac/1802_Fold Paper.cpp
+++ Solved.ac/Solved.ac/1802_Fold Paper.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,6 +7,24 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+
+	// 종이 접기
+
+	/*
+	 * T : 테스트 케이스 수
+	 */
+
+	int T; cin >> T;
+
+	for(int i=0; i<T; ++i)
+	{
+		// 종이의 정보는 문자열로 주어진다
+		// string or const char*
+		// 1 : OUT / 0 : IN
+
+		string foldInfo; cin >> foldInfo;
+
+	}
 
 
 
