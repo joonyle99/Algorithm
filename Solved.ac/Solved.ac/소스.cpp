8ac51989618fc02ff57276959454458589ed7458
joonyle99 <gongjunyeol@gmail.com>
2023-11-07 16:48:34 +0900

    multiprecision

---
+++ Solved.ac/Solved.ac/소스.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	char nArr[1001];
+	char mArr[1001];
+
+	cin >> nArr;
+	cin >> mArr;
+
+	cout << nArr << endl;
+	cout << mArr << endl;
+
+
+
+	return 0;
+}
