3ab8e4b2a5a7064657bb6846a8082965ebf27f43
joonyle99 <gongjunyeol@gmail.com>
2023-12-03 23:37:47 +0900

    for, while 코드 스니펫 추가 및 BFS 영역 구하기 문제 푸는중

--- Solved.ac/Solved.ac/2583_Calculate Area.cpp
+++ Solved.ac/Solved.ac/2583_Calculate Area.cpp
@@ -10,13 +10,30 @@ using std::cout;
 
 int main()
 {
-    // Break the ios for C and C++
-    std::ios::sync_with_stdio(false);
+	// Break the ios for C and C++
+	std::ios::sync_with_stdio(false);
 
-    // Untie the streams that bind cin and cout (Output cout before cin's buffer is empty)
-    std::cin.tie(nullptr);
+	// Untie the streams that bind cin and cout (Output cout before cin's buffer is empty)
+	std::cin.tie(nullptr);
 
-    // Title :
+	// Title : 영역 구하기
 
-    return 0;
+	int M, N, K;
+	cin >> M >> N >> K;
+
+	std::vector<std::vector<int>> matrix(M + 1);
+
+	for (int i = 0; i < matrix.size(); ++i)
+		matrix[i].resize(N + 1);
+
+	while (K-- > 0)
+	{
+		int bl_x, bl_y, tr_x, tr_y;
+		cin >> bl_x >> bl_y >> tr_x >> tr_y;
+
+		matrix[][];
+	}
+
+
+	return 0;
 }
