57276bb76c66152ca2d285af0faf077a2249e9ae
joonyle99 <gongjunyeol@gmail.com>
2023-12-04 11:38:55 +0900

    Y축 반전 좌표 + 사각형의 중앙이 아닌 좌하단 우상단 좌표계로 복잡한 BFS문제 해결
    + For Loop 코드 스니펫 수정

--- Solved.ac/Solved.ac/2583_Calculate Area.cpp
+++ Solved.ac/Solved.ac/2583_Calculate Area.cpp
@@ -8,6 +8,11 @@
 using std::cin;
 using std::cout;
 
+int M, N, K;
+
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1, 0,0 };
+
 struct Position
 {
 	int x, y;
@@ -15,6 +20,40 @@ struct Position
 		: x(_x), y(_y)
 	{}
 };
+
+int visited[101][101];
+
+int BFS(Position startPos)
+{
+	int innerCount = 1;
+
+	std::queue<Position> myQueue;
+	myQueue.push(startPos);
+	visited[startPos.y][startPos.x] = 1;
+
+	while (!myQueue.empty())
+	{
+		Position curPos = myQueue.front();
+		myQueue.pop();
+
+		for (int k = 0; k < 4; ++k)
+		{
+			Position nextPos(curPos.x + dx[k], curPos.y + dy[k]);
+
+			if (nextPos.x < 0 || nextPos.y < 0 || nextPos.x >= N || nextPos.y >= M)
+				continue;
+
+			if (visited[nextPos.y][nextPos.x] != 1)
+			{
+				visited[nextPos.y][nextPos.x] = 1;
+				myQueue.push(nextPos);
+				innerCount++;
+			}
+		}
+	}
+
+	return innerCount;
+}
 
 int main()
 {
@@ -26,34 +65,56 @@ int main()
 
 	// Title : 영역 구하기
 
-	int M, N, K;
 	cin >> M >> N >> K;
 
-	std::vector<std::vector<int>> matrix(M + 1);
-
-	for (int i = 0; i < matrix.size(); ++i)
-		matrix[i].resize(N + 1);
-
+	// 모눈종이에 K개의 직사각형을 그린다
 	while (K-- > 0)
 	{
 		int bl_x, bl_y, tr_x, tr_y;
 		cin >> bl_x >> bl_y >> tr_x >> tr_y;
 
-		Position bl(bl_x, bl_y);
-		Position tr(tr_x, tr_y);
-
-		for (int i = bl_x; i < tr_x; ++i)
+		for (int x = bl_x; x < tr_x; ++x)
 		{
-			for (int j = bl_y; j < tr_y; ++j)
+			for (int y = bl_y; y < tr_y; ++y)
 			{
-
+				visited[y][x] = 1;
 			}
-
 		}
-
-
 	}
 
+	/*
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			cout << visited[i][j] << " ";
+		}
+		cout << '\n';
+	}
+	*/
+
+	int outterCount = 0;
+	std::vector<int> innerCountArr;
+
+	for (int y = 0; y < M; ++y)
+	{
+		for (int x = 0; x < N; ++x)
+		{
+			Position curPos(x, y);
+			if (!visited[curPos.y][curPos.x])
+			{
+				innerCountArr.push_back(BFS(curPos));
+				outterCount++;
+			}
+		}
+	}
+
+	cout << outterCount << '\n';
+
+	std::sort(innerCountArr.begin(), innerCountArr.end());
+
+	for (const auto count : innerCountArr)
+		cout << count << " ";
 
 	return 0;
 }
