2547052f8b724bb20302fcfc9520d50e9a8ba337
joonyle99 <gongjunyeol@gmail.com>
2024-06-09 17:53:27 +0900

    you have to solve the "zelda problem" with dijkstra algorithm

---
+++ Solved.ac/Solved.ac/4485_zelda.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+
+const int MAX_SIZE = 130;
+
+int cave[MAX_SIZE][MAX_SIZE];
+bool visited[MAX_SIZE][MAX_SIZE];
+int dist[MAX_SIZE][MAX_SIZE];
+
+void Dijkstra_Function()
+{
+
+}
+
+int main()
+{
+    // Break the ios for C and C++
+    std::ios::sync_with_stdio(false);
+
+    // Untie the streams that bind cin and cout (Output cout before cin's buffer is empty)
+    std::cin.tie(nullptr);
+
+    // Title : 녹색 옷 입은 애가 젤다지?
+
+    while (true)
+    {
+        cin >> N;
+
+        if (N == 0)
+            break;
+
+        // i = N (세로), j = N (가로)
+        // 이렇게 하므로써, 전치행렬화 한다
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int k; cin >> k;
+                cave[j][i] = k;
+            }
+        }
+
+        // 최단 거리 초기화
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dist[j][i] = cave[j][i];
+            }
+        }
+
+        // dijksta는 start 노드를 기점으로 모든 노드까지의 최단 거리를 구하는 알고리즘이다.
+        // Dijkstra_Function(start);
+
+        // [0, 0] -> [N-1, N-1]
+        cout << "Problem " << N + 1 << ": " << dist[N - 1][N - 1] << '\n';
+    }
+
+    return 0;
+}
