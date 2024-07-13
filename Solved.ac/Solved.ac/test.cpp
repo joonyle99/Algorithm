e5bc03565052a67daee939e28dea48e70ca086ef
Junyeol Gong <gongjunyeol@gmail.com>
2024-07-13 18:57:06 +0900

    .

--- Solved.ac/Solved.ac/test.cpp
+++ Solved.ac/Solved.ac/test.cpp
@@ -9,9 +9,22 @@ int main()
 	// Untie the streams that bind cin and cout (Output cout before cin's buffer is empty)
 	std::cin.tie(nullptr);
 
-	// Title : 옥상 정원 꾸미기
+#ifdef _DEBUG
+	cout << "<디버그 모드>" << "\n\n";
+#else
+	cout << "<릴리즈 모드>" << "\n\n";
+#endif
 
+	int a = 10;
+	int b = 20;
 
+	std::cout << "변수 a의 주소: " << &a << '\n';
+	std::cout << "변수 b의 주소: " << &b << "\n\n";
+
+	// 주소 차이 계산
+	ptrdiff_t diff = reinterpret_cast<char*>(&b) - reinterpret_cast<char*>(&a);
+
+	std::cout << "두 변수 사이의 메모리 주소 차이: " << diff << " 바이트" << '\n';
 
 	return 0;
 }
