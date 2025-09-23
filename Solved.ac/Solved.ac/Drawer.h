b480d2682395022f3cfe8beb6ce76afbd727ccb5
joonyle99 <gongjunyeol@gmail.com>
2025-09-23 13:17:31 +0900

    #Drawer 클래스 개선
    - 배열 Print 함수 추가 (1차, 2차 배열)

--- Solved.ac/Solved.ac/Drawer.h
+++ Solved.ac/Solved.ac/Drawer.h
@@ -9,7 +9,7 @@ private:
     static Drawer* Instance;
 
 public:
-    // Áö¿¬ ÃÊ±âÈ­ ¹æ½Ä
+    // ì§€ì—° ì´ˆê¸°í™” ë°©ì‹
     inline static Drawer& GetInstance() {
         if (Instance == nullptr)
             Instance = new Drawer;
@@ -17,7 +17,50 @@ public:
     }
 
 public:
-	inline void PrintVector(std::vector<int>& vec) {
+	inline void PrintArray(int arr[], int len) {
+        std::cout << "============= PrintArray =============" << '\n';
+
+        std::cout << "[ ";
+
+        for (int i = 0; i < len; i++)
+        {
+            if (i == len - 1)
+            {
+                std::cout << arr[i];
+            }
+            else
+            {
+                std::cout << arr[i] << ", ";
+            }
+        }
+
+        std::cout << " ]" << '\n';
+	}
+    inline void PrintArray2(int arr[][10], int row, int col) {
+        std::cout << "============= PrintArray2 =============" << '\n';
+
+        for (int i = 0; i < row; i++) {
+
+            std::cout << "[ ";
+
+            for (int j = 0; j < col; j++)
+            {
+                if (j == col - 1)
+                {
+                    std::cout << arr[i][j];
+                }
+                else
+                {
+                    std::cout << arr[i][j] << ", ";
+                }
+            }
+
+            std::cout << " ]" << '\n';
+        }
+    }
+    inline void PrintVector(std::vector<int>& vec) {
+        std::cout << "============= PrintVector =============" << '\n';
+
         std::cout << "[ ";
 
         for (int i = 0; i < vec.size(); i++)
@@ -33,5 +76,5 @@ public:
         }
 
         std::cout << " ]" << '\n';
-	}
+    }
 };
